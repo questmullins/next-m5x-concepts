bd262ddbfc781895f370d65a07928747f486b74f
questmullins <questmullins@gmail.com>
2026-05-29 14:05:21 -0700

    refresh10

---
+++ next-m5x-concepts/app/layout.js
@@ -0,0 +1,22 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "M5X Mechanix",
+  description: "Glass Colorful concept for M5X Mechanix."
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
